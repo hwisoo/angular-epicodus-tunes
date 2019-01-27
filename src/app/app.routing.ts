953f42b62ff61eb331d6159707d0f724f19c280d
James Cho <hwisoocho@gmail.com>
2019-01-26 16:40:33 -0800

    add dynamic routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { Album } from './album.model';
+
 const appRoutes: Routes = [
   {
     path: '',
@@ -15,6 +18,10 @@ const appRoutes: Routes = [
   {
     path: 'marketplace',
     component: MarketplaceComponent
+  },
+  {
+    path: 'albums/:id',
+    component: AlbumDetailComponent
   }
 ];
 
