79f3459ec560fc0376e046765b6bb976afbee06f
James Cho <hwisoocho@gmail.com>
2019-01-29 22:29:58 -0800

    adding database to firebase

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
 import { AlbumService } from '../album.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-marketplace',
@@ -10,9 +11,9 @@ import { AlbumService } from '../album.service';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
-
+  albums: FirebaseListObservable<any[]>;
   constructor(private router: Router, private albumService: AlbumService) { }
-  albums: Album[];
+
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
